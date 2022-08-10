5bb24b6f10df16c52b384fc0ba37d7b7d4247e67
Vladyas <yaskevich@mail.ru>
2022-08-10 08:45:07 +0300

    docstrings and comments are added

--- command_modem.py
+++ command_modem.py
@@ -1,3 +1,13 @@
+"""
+Module fuctions automate web interface actions in D-Link DIR-651 web manager application
+(which is found at http://192.168.0.1)
+
+use parameters (specified in config.py):
+-r to restart modem
+-w to toggle Wi-Fi
+
+change login and password consts in config.p to pass login in
+"""
 import sys, time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -11,6 +21,7 @@ from config import *
 
 
 def login_modem(driver, path):
+    """Pass login dilaog with login/password specified in config.py"""
     driver.get(path)
     elem_username = driver.find_element(By.ID, FIELD_LOGIN_NAME)
     elem_username.send_keys(LOGIN_NAME)
@@ -20,18 +31,17 @@ def login_modem(driver, path):
 
 
 def restart_modem(driver):
+    """Click on Restart modem and accept the action in alert window"""
     button_reboot = driver.find_element(By.XPATH, BUTTON_RESTART)
     button_reboot.click()
     driver.switch_to.alert.accept()
 
 
 def toggle_wifi(driver):
+    """Click on Wi-Fi checkbox and Apply button"""
     time.sleep(8)
-
     checkbox_wifi = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.XPATH, CHECKBOX_WIFI)))
-
     checkbox_wifi.click()
-
     button_apply = driver.find_element(By.XPATH, BUTTON_APPLY_WIFI)
     button_apply.click()
 
--- config.py
+++ config.py
@@ -1,14 +1,28 @@
+"""
+web interface automatization constants for D-Link DIR-651 web manager application
+For more info, look directly to config.py code. All constants names are intuitive.
+Don't forget to change login and password!
+"""
+
 # All settings are done for D-Link Dir-651
 # MODEM ADMIN LOGIN DIALOG
+# login and password fields full XPATHs. Could be found by Chrome DevTool.
 FIELD_LOGIN_NAME = "A1"
 FIELD_LOGIN_PWD = "A2"
+# login and password
 LOGIN_NAME = "admin"
 LOGIN_PWD = "**************"
+
 #RESTART MODEM page and button
+# Modem restart page address
 URL_RESTART_MODEM = "http://192.168.0.1/#system/sysconf"
+# Restart button full XPATH. Could be found by Chrome DevTool.
 BUTTON_RESTART = "/html/body/div[5]/div[2]/div[2]/div[2]/div[3]/div[2]/div/div[2]/div[1]/div[1]/div[2]/div/div/div[1]"
+
 #WI-FI settings page
+# Wi-Fi settings page address
 URL_TOGGLE_WIFI = "http://192.168.0.1/#wifi/basic"
+# Wi-Fi checkbox and Apply controls full XPATHs. Could be found by Chrome DevTool.
 CHECKBOX_WIFI = "/html/body/div[5]/div[2]/div[2]/div[2]/div[3]/div[2]/div/div/div[1]/div[1]/div[1]/div[1]/div[2]/input"
 BUTTON_APPLY_WIFI = "/html/body/div[5]/div[2]/div[2]/div[2]/div[3]/div[3]/div[3]/div/div/div[1]"
 
